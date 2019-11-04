53c7b60e4f41673eae063301fd542d94e7a586cf
None <akshatsharma46@gmail.com>
2019-11-04 00:36:57 -0800

    mn_init: clear previous topologies before initializing new ones

--- mn_scripts/mn_init.sh
+++ mn_scripts/mn_init.sh
@@ -1,1 +1,4 @@
+echo "************CLEANING PREVIOUS TOPOLOGY*************"
+eval "sudo mn -c"
+echo "****************CREATING TOPOLOGY******************"
 eval "sudo mn --controller=remote,ip=$1 --custom=topology.py --topo=mytopo --switch=ovsk,protocol=OpenFlow13"
