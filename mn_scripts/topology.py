b5274465225889a2b38462fd23c51548041c97e9
akshat <akshatsharma46@gmail.com>
2019-11-28 18:35:47 -0800

    changed topology script to create topology as well

--- mn_scripts/topology.py
+++ mn_scripts/topology.py
@@ -1,27 +1,79 @@
+# from mininet.topo import Topo
+# class MyTopo(Topo):
+# 	"Topology."
+# 	def __init__(self):
+# 		"Creating custom topo."
+# 		Topo.__init__(self)
+# 		s1 = self.addSwitch('s1', ip='10.0.1.1')
+# 		s2 = self.addSwitch('s2', ip='10.0.2.1')
+# 		s3 = self.addSwitch('s3', ip='10.0.3.1')
+# 		s4 = self.addSwitch('s4', ip='10.0.4.1')
+# 		h11 = self.addHost('h11', ip='10.0.1.10')
+# 		h21 = self.addHost('h21', ip='10.0.2.10')
+# 		h31 = self.addHost('h31', ip='10.0.3.10')
+# 		h41 = self.addHost('h41', ip='10.0.4.10')
+
+# 		self.addLink(h11, s1)
+# 		self.addLink(h21, s2)
+# 		self.addLink(h31, s3)
+# 		self.addLink(h41, s4)
+
+# 		self.addLink(s1, s2)
+# 		self.addLink(s1, s3)
+# 		self.addLink(s1, s4)
+# 		self.addLink(s2, s3)
+# 		self.addLink(s2, s4)
+# 		self.addLink(s3, s4)
+# topos = {'mytopo': (lambda: MyTopo())}
+
+#!/usr/bin/python
+
 from mininet.topo import Topo
-class MyTopo(Topo):
-	"Topology."
-	def __init__(self):
-		"Creating custom topo."
-		Topo.__init__(self)
-		Switch1 = self.addSwitch('s1', ip='10.0.1.1')
-		Switch2 = self.addSwitch('s2', ip='10.0.2.1')
-		Switch3 = self.addSwitch('s3', ip='10.0.3.1')
-		Switch4 = self.addSwitch('s4', ip='10.0.4.1')
-		Host1 = self.addHost('h11', ip='10.0.1.10')
-		Host2 = self.addHost('h21', ip='10.0.2.10')
-		Host3 = self.addHost('h31', ip='10.0.3.10')
-		Host4 = self.addHost('h41', ip='10.0.4.10')
+from mininet.net import Mininet
+from mininet.node import CPULimitedHost
+from mininet.link import TCLink
+from mininet.util import dumpNodeConnections
+from mininet.log import setLogLevel
 
-		self.addLink(Host1, Switch1)
-		self.addLink(Host2, Switch2)
-		self.addLink(Host3, Switch3)
-		self.addLink(Host4, Switch4)
+class CreateTopo( Topo ):
+    "Single switch connected to n hosts."
+    def build( self, n=4 ):
+		s1 = self.addSwitch('s1', ip='10.0.1.1')
+		s2 = self.addSwitch('s2', ip='10.0.2.1')
+		s3 = self.addSwitch('s3', ip='10.0.3.1')
+		s4 = self.addSwitch('s4', ip='10.0.4.1')
+		h11 = self.addHost('h11', ip='10.0.1.10')
+		h21 = self.addHost('h21', ip='10.0.2.10')
+		h31 = self.addHost('h31', ip='10.0.3.10')
+		h41 = self.addHost('h41', ip='10.0.4.10')
+		
+		self.addLink(h11, s1)
+		self.addLink(h21, s2)
+		self.addLink(h31, s3)
+		self.addLink(h41, s4)
 
-		self.addLink(Switch1, Switch2)
-		self.addLink(Switch1, Switch3)
-		self.addLink(Switch1, Switch4)
-		self.addLink(Switch2, Switch3)
-		self.addLink(Switch2, Switch4)
-		self.addLink(Switch3, Switch4)
-topos = {'mytopo': (lambda: MyTopo())}
+		self.addLink(s1, s2)
+		self.addLink(s1, s3)
+		self.addLink(s1, s4)
+		self.addLink(s2, s3)
+		self.addLink(s2, s4)
+		self.addLink(s3, s4)
+
+def perfTest():
+    "Create network and run simple performance test"
+    topo = CreateTopo( n=4 )
+    net = Mininet( topo=topo,
+	           host=CPULimitedHost, link=TCLink )
+    net.start()
+    print "Dumping host connections"
+    dumpNodeConnections( net.hosts )
+    print "Testing network connectivity"
+    net.pingAll()
+    # print "Testing bandwidth between h1 and h4"
+    # h1, h4 = net.get( 'h1', 'h4' )
+    # net.iperf( (h1, h4) )
+    net.stop()
+
+if __name__ == '__main__':
+    setLogLevel( 'info' )
+    perfTest()
